cee7842402f174bc0960a6a91ca7cefc51cfd3c0
Monique <meg.emcube0828@gmail.com>
2018-07-10 14:03:21 +0800

    edits

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-var modal = document.getElementById('show-my-interests');
-
+var intmodal = document.getElementById('show-my-interests');
+var projmodal = document.getElementById('show-my-projects');
 // Get the button that opens the modal
-var btn = document.getElementById("interest");
+var intbtn = document.getElementById("interests");
+var projbtn = document.getElementById("projects");
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+intbtn.onclick = function() {
 	$('body').css('overflow','hidden')
-    modal.style.display = "block";
+    intmodal.style.display = "block";
 }
-
+projbtn.onclick = function() {
+	$('body').css('overflow','hidden')
+    intmodal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
 	$('body').css('overflow','scroll')
-    modal.style.display = "none";
+    intmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == intmodal) {
     	$('body').css('overflow','scroll')
-        modal.style.display = "none";
+        intmodal.style.display = "none";
     }
 }
